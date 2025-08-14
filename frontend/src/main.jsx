08384377e462522876a9667ee0235a99e565aca1
Josue <madridjosue506@gmail.com>
2025-08-14 15:36:05 -0600

    Fix: Corrected import path for App.jsx in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 // frontend/src/main.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js'; // Asegúrate de que apunte a App.jsx
+import App from './App.jsx'; // Asegúrate de que apunte a App.jsx
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
